485d5ca48539551180ae1331a94531260c12d12d
Mohd Iqbal <iqbal.safian@gmail.com>
2018-07-15 13:51:05 +0800

    Create test scenario for runProcess method

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 export default class LED {
-  runProcess(){}
-  setInput(fileName){}
+  constructor(){}
+  runProcess(fileName = null){
+    let fs = require('fs')
+    if (fileName === null) {
+      console.log('File name not found')
+      return
+    }
+    this.setInput(fileName)
+  }
+  setInput(fileName){
+    let currentIndex = 0
+    let theObject = {}
+    let lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream(fileName)
+    })
+    lineReader.on('line', (line) => {
+      console.log(line);
+      if (line == null) {
+        this.processInput(theObject)
+      } else {
+        theObject[currentIndex] = line
+      }
+    })
+  }
   processInput(theObject){}
   logOutput(theObject){}
 }
--- index.test.js
+++ index.test.js
@@ -1,1 +1,16 @@
 import LED from './index'
+
+it('Will check if the LED class has been init by calling its constructor', ()=>{
+  const Led = new LED()
+  expect(Led).toBeTruthy()
+})
+
+it('Expects if the method runProcess is being called', () => {
+  const Led = new LED()
+  Led.runProcess('sample-input.txt')
+})
+
+it('Will log an error if no file name being passed to the method runProcess', () => {
+  const Led = new LED()
+  Led.runProcess()
+})
