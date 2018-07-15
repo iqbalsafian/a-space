dd4ba566cc878fbf113b18050bf94cec7a80940f
Mohd Iqbal <iqbal.safian@gmail.com>
2018-07-15 16:47:40 +0800

    Put processInput method to the test

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 export default class LED {
   constructor(){}
   runProcess(fileName = null){
-    let fs = require('fs')
-    if (fileName === null) {
-      console.log('File name not found')
-      return
-    }
-    this.setInput(fileName)
-  }
-  setInput(fileName){
     let currentIndex = 0
     let theObject = {}
-    let lineReader = require('readline').createInterface({
-      input: require('fs').createReadStream(fileName)
-    })
-    lineReader.on('line', (line) => {
-      console.log(line);
-      if (line == null) {
+
+    try {
+      let lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(fileName)
+      })
+      lineReader.on('line', (line) => {
+        if (line.length === 0) {
+          this.processInput(theObject)
+          currentIndex = 0
+        } else {
+          theObject[currentIndex] = line
+        }
+        currentIndex++
+      })
+      lineReader.on('close', ()=>{
         this.processInput(theObject)
-      } else {
-        theObject[currentIndex] = line
-      }
-    })
+      })
+    } catch(err) {
+      console.log('File not found');
+      return
+    }
   }
-  processInput(theObject){}
-  logOutput(theObject){}
+  processInput(theObject){
+    let receivedNumbers = (theObject[0].length / 4).toFixed();
+    let numberList = ''
+
+    for (let i = 1; i <= receivedNumbers; i++) {
+      let tempObject = {data:{0: {0:0, 1:0, 2:0}, 1: {0:0, 1:0, 2:0}, 2: {0:0, 1:0, 2:0}}, filled: 0}
+      let filled = 0;
+      for (let j = 0; j < 3; j++) {
+        for (let k = (i * 3 - 4 + i); k < (i * 3 + i); k++) {
+          if (theObject[j][k] !== ' ') {
+            filled++;
+              tempObject['data'][j][k] = 1
+          }
+        }
+      }
+      tempObject['filled'] = filled
+      numberList += this.logOutput(tempObject)
+    }
+    console.log(numberList);
+  }
+  logOutput(theObject){
+    return theObject['filled']
+  }
 }
+
+let Led = new LED()
+Led.runProcess('sample-input.txt')
--- index.test.js
+++ index.test.js
@@ -5,12 +5,24 @@ it('Will check if the LED class has been init by calling its constructor', ()=>{
   expect(Led).toBeTruthy()
 })
 
+it('Will log an error if no file name being passed to the method runProcess', () => {
+  const Led = new LED()
+  Led.runProcess()
+})
+
 it('Expects if the method runProcess is being called', () => {
   const Led = new LED()
   Led.runProcess('sample-input.txt')
 })
 
-it('Will log an error if no file name being passed to the method runProcess', () => {
+it('Should log output when processInput method is called', () => {
   const Led = new LED()
-  Led.runProcess()
+  const mockObj = {0: ' - ', 1: '| |', 2: ' _ '}
+  Led.processInput(mockObj)
+})
+
+it('Object "one" should return a number', () => {
+  const Led = new LED()
+  const mockObj = {filled: 2, data: {0: {0: 0, 1: 0, 2: 0}, 1: {0:1, 1:0, 2:0}, 2: {0:1, 1:0, 2:0}}}
+  expect(Led.logOutput(mockObj)).toBe(2)
 })
